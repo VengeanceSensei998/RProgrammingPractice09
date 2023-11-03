efebfe4199df5e96d717492a0c8a6bda1f744984
Jonas <jonastheitguy@gmail.com>
2023-11-03 23:29:13 +0800

    Added first R Programming Exercise

---
+++ Exercise 1 Creating an object in R.R
@@ -0,0 +1,26 @@
+#Exercise 1 Creating an object in R 
+#Question:
+
+#Create a vector x storing the sequence 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20.
+x = 10:20
+#Alternative Method : x <- c(10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+x
+#Add 2 to it. Save the result into a variable called y.
+y = x + 2
+y
+
+#Multiply y by 3. Save that into a variable called z.
+z = y * 3
+z
+#Subtract 6 from z and divide the result by 3. Save what you get into a variable called answer.
+z = (z-6)/3
+z
+#Print your answer variable.
+z
+#Try to do the entire operation in a single line of code.
+inaline = (((x + 2) * 2) - 6)/3
+inaline
+
+
+
+
